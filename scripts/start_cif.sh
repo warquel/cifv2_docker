0db3eeda09d4ead473c61fe261c0e0460e6095f2
Warren Raquel <wraquel@illinois.edu>
2016-06-03 11:01:38 -0500

    Updated todo and newbie startup script.

--- scripts/start_cif.sh
+++ scripts/start_cif.sh
@@ -1,13 +1,22 @@
 #!/bin/sh
 
-IMAGE="warquel/cifv2"
-ESVOL="esdata"
+IMAGE="warquel/cifv2_docker:latest"
+BASEDIR=${HOME}
+ESVOL="-v ${BASEDIR}/cif/esdata:/usr/share/elasticsearch/data"
+CONFVOL="-v ${BASEDIR}/cif/conf:/opt/cif/etc"
+NAME="cifv2"
+IP="$(docker-machine ip)"
 
-MACHINE_ID="$(docker run -d -v esdata:/usr/share/elasticsearch/data -p 8443:443 -p 5000:5000 -p 9200:9200 warquel/cifv2)"
+CMD="docker run --name ${NAME} -d ${ESVOL} ${CONFVOL} -p 8080:80 -p 8443:443 -p 5000:5000 -p 9200:9200 ${IMAGE}"
+
+echo "Running: ${CMD}"
+MACHINE_ID="$(${CMD})"
 
 echo "      Machine ID: ${MACHINE_ID}"
 echo "Short Machine ID: ${MACHINE_ID:0:12}"
-echo "    Command Line: docker exec -ti ${MACHINE_ID:0:12} /bin/bash"
+echo "    Command Line: docker exec -ti ${NAME} /bin/bash"
 echo "Run the following commands shortly after starting the container for the first time"
-echo "   Initialize ES: docker exec -ti ${MACHINE_ID:0:12} /opt/cif/bin/cif.ops init"
-echo "   Initialize ES: docker exec -ti ${MACHINE_ID:0:12} /opt/cif/bin/cif.ops generate-tokens"
+echo "   Initialize ES: docker exec -ti ${NAME} /opt/cif/bin/cif.ops init"
+echo "   Initialize ES: docker exec -ti ${NAME} /opt/cif/bin/cif.ops generate-tokens"
+echo ""
+echo "CIFv2: http://${IP}:8080" 
